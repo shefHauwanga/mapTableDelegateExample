f169e43955580de58281ebdf642b347acf3447c9
sheefeni <sheefeni@sheefenis-imac.local>
2017-07-05 17:04:47 -0400

    Initial commit. Removed supplied viewController and replaced it with ParentContainerController. Made sure that class for initial view controller was ParentContainerController

---
+++ mapToListOne/Controllers/ParentContainerController.swift
@@ -0,0 +1,16 @@
+//
+//  ParentContainerController.swift
+//  mapToListOne
+//
+//  Created by sheefeni on 7/5/17.
+//  Copyright © 2017 ShefCooksCode. All rights reserved.
+//
+
+import UIKit
+
+class ParentContainerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
